637bca5cd0c59934ad0708e6d76b16e141cd5299
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-08-21 17:17:19 +0900

    Create player controller and player manager script

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private Transform			playerTransform;            // 플레이어 트랜스폼
+	[SerializeField]
+	private Rigidbody2D			playerRigidbody2D;          // 플레이어 리지드바디 2D
+
+	// 수치
+	public	int					originJumpCount;			// 기본 점프 카운트
+
+	[SerializeField]
+	private float				moveSpeed;                  // 움직임 속도
+	[SerializeField]
+	private float				jumpPower;                  // 점프 파워
+
+	// 인스펙터 비노출 변수
+	// 수치
+	private int					jumpCount;                  // 점프 카운트
+	private float				horizontalMove;             // 좌우 이동
+	private bool				isJumping = false;			// 점프 여부
+
+
+	// 초기화
+	private void Awake()
+	{
+		if (playerTransform == null)
+		{
+			playerTransform = transform;
+		}
+
+		if (playerRigidbody2D == null)
+		{
+			playerRigidbody2D = GetComponent<Rigidbody2D>();
+		}
+
+		ResetJump();
+	}
+
+	// 프레임
+	private void Update()
+	{
+		horizontalMove = Input.GetAxis("Horizontal");
+
+		if (Input.GetButtonDown("Jump") && jumpCount > 0)
+		{
+			isJumping = true;
+		}
+	}
+
+	// 고정 프레임
+	private void FixedUpdate()
+	{
+		Run();
+		Jump();
+	}
+
+	// 움직임
+	private void Run()
+	{
+		Vector2 position = playerTransform.position;
+
+		position.x += horizontalMove * Time.deltaTime * moveSpeed;
+		playerTransform.position = position;
+
+	}
+
+	// 점프
+	private void Jump()
+	{
+		if (isJumping)
+		{
+			playerRigidbody2D.velocity = Vector2.zero;
+			playerRigidbody2D.AddForce(Vector2.up * jumpPower * Input.GetAxis("Jump"));
+
+			jumpCount--;
+			isJumping = false;
+		}
+	}
+
+	// 점프 리셋
+	public void ResetJump()
+	{
+		jumpCount = originJumpCount;
+	}
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private PlayerController				playerControl;              // 플레이어 컨트롤
+
+
+	// 초기화
+	private void Awake()
+	{
+		if (playerControl == null)
+		{
+			playerControl = GetComponent<PlayerController>();
+		}
+	}
+
+	// 충돌체 진입
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// 블록일 경우
+		if (collision.gameObject.layer == 8)
+		{
+			playerControl.ResetJump();
+		}
+	}
+}
