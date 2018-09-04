a7e1c57c27774ff32f759ddd7fd39862301b6135
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-09-04 19:27:00 +0900

    Add custom block rotation function

--- Assets/Scripts/Player/CustomBlockGenerator.cs
+++ Assets/Scripts/Player/CustomBlockGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CustomBlockGenerator: MonoBehaviour
 {
@@ -11,16 +12,33 @@ public class CustomBlockGenerator: MonoBehaviour
 	public	bool					canCreate = true;	                // 생성 가능상태인지
 
 	[SerializeField]
-	private float					createZPosition;					// 생성 z포지션
+	private float					createZPosition;                    // 생성 z포지션
+
+	// 인스펙터 비노출 변수
+	// 일반
+	private Transform				targetBlock = null;                 // 생성한 블럭
 
 
 	// 프레임
 	private void Update()
 	{
-		// 클릭
+		// 클릭 시작
 		if (Input.GetMouseButtonDown(0) && canCreate)
 		{
 			CreateBlock();
+		}
+
+		// 클릭 중
+		if (Input.GetMouseButton(0) && targetBlock != null)
+		{
+			StartCoroutine("ClickTimer");
+		}
+
+		// 클릭 끝
+		if (Input.GetMouseButtonUp(0))
+		{
+			StopCoroutine("ClickTimer");
+			targetBlock = null;
 		}
 	}
 
@@ -28,8 +46,33 @@ public class CustomBlockGenerator: MonoBehaviour
 	private void CreateBlock()
 	{
 		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+		
 		position.z = createZPosition;
-		Instantiate(customBlockPrefab, position, Quaternion.identity, transform);
+		targetBlock = Instantiate(customBlockPrefab, position, Quaternion.identity, transform).transform;
+	}
+
+	// 블럭 회전
+	private void RotateBlock()
+	{
+		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 targetPos;
+
+		targetPos.x = position.x - targetBlock.position.x;
+		targetPos.y = position.y - targetBlock.position.y;
+
+		float angle = -1 * Mathf.Rad2Deg * Mathf.Atan2(targetPos.x, targetPos.y) + 90;
+
+		targetBlock.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+	}
+
+	// 클릭 시간 코루틴
+	private IEnumerator ClickTimer()
+	{
+		yield return new WaitForSeconds(1f);
+
+		if (Input.GetMouseButton(0) && targetBlock != null)
+		{
+			RotateBlock();
+		}
 	}
 }
