dc3efbe7134d784d3985a276e101257c53735d6a
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-09-27 22:11:57 +0900

    Add bomb object

---
+++ Assets/Scripts/Object/Item/Bomb.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Bomb : MonoBehaviour
+{
+	// 인스펙터 비노출 변수
+	// 일반
+	private Tilemap tileMap;
+	private Grid	grid;
+
+
+	// 초기화
+	private void Awake()
+	{
+		GameObject target = GameObject.Find("Sub");
+
+		grid = target.transform.parent.GetComponent<Grid>();
+		tileMap = target.GetComponent<Tilemap>();
+	}
+
+	// 오브젝트 온
+	private void OnEnable()
+	{
+		StartCoroutine("BombCountDown");
+	}
+
+	// 충돌체 진입
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Block"))
+		{
+			StopCoroutine("BombCountDown");
+			ComitBomb();
+		}
+	}
+
+	// 폭발
+	private void ComitBomb()
+	{
+		Vector3Int core = grid.WorldToCell(transform.position);
+
+		for (int i = -2; i <= 2; i++)
+		{
+			for (int j = -2; j <= 2; j++)
+			{
+				tileMap.SetTile(new Vector3Int(core.x + i, core.y + j, 0), null);
+			}
+		}
+
+		ObjectPoolManager.Release("Bomb", gameObject);
+	}
+
+	// 폭발 코루틴
+	private IEnumerator BombCountDown()
+	{
+		yield return new WaitForSeconds(1f);
+
+		ComitBomb();
+	}
+}
