5c99f0754e76a61f9ee1bc01c9a04a52d60f16d9
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-01 22:12:14 +0900

    Add random map generate system

---
+++ Assets/Scripts/System/RandomMapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RandomMapGenerator : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private		GameObject[]		mapPattern;         // 맵 패턴
+
+
+	// 초기화
+	private void Awake()
+	{
+		MapCreate();
+	}
+
+	// 맵 생성
+	public void MapCreate()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Instantiate(mapPattern[Random.Range(0, mapPattern.Length)], new Vector2(0, 70 * i), Quaternion.identity);
+		}
+	}
+}
