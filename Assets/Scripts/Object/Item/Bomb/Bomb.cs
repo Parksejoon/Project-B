c983ce66b83a4db9bff578ec936385e450f72ec9
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-05 14:49:12 +0900

    Change bomb object find system

--- Assets/Scripts/Object/Item/Bomb/Bomb.cs
+++ Assets/Scripts/Object/Item/Bomb/Bomb.cs
@@ -12,17 +12,23 @@ public class Bomb : MonoBehaviour
 
 	// 인스펙터 비노출 변수
 	// 일반
-	private Tilemap		tileMap;			// 타일맵
-	private Grid		grid;				// 그리드
+	private Tilemap[]	tileMaps;			// 타일맵들
+	private Grid[]		grids;				// 그리드들
 
 
 	// 초기화
 	private void Awake()
 	{
-		GameObject targetGrid = ClosestObject("SoilBlock");
-		
-		grid = targetGrid.transform.parent.GetComponent<Grid>();
-		tileMap = targetGrid.GetComponent<Tilemap>();
+		GameObject[] targetGrids = GameObject.FindGameObjectsWithTag("SoilBlock");
+
+		tileMaps = new Tilemap[targetGrids.Length];
+		grids = new Grid[targetGrids.Length];
+
+		for (int i = 0; i < targetGrids.Length; i++)
+		{
+			grids[i] = targetGrids[i].transform.parent.GetComponent<Grid>();
+			tileMaps[i] = targetGrids[i].GetComponent<Tilemap>();
+		}
 	}
 
 	// 시작
@@ -34,13 +40,16 @@ public class Bomb : MonoBehaviour
 	// 폭발
 	private void ComitBomb()
 	{
-		Vector3Int core = grid.WorldToCell(transform.position);
-
-		for (int i = -4; i <= 4; i++)
+		for (int t = 0; t < tileMaps.Length; t++)
 		{
-			for (int j = -4; j <= 4; j++)
+			Vector3Int core = grids[t].WorldToCell(transform.position);
+
+			for (int i = -4; i <= 4; i++)
 			{
-				tileMap.SetTile(new Vector3Int(core.x + i, core.y + j, 0), null);
+				for (int j = -4; j <= 4; j++)
+				{
+					tileMaps[t].SetTile(new Vector3Int(core.x + i, core.y + j, 0), null);
+				}
 			}
 		}
 
@@ -49,30 +58,7 @@ public class Bomb : MonoBehaviour
 
 		Destroy(gameObject);
 	}
-
-	// 가장 가까운 오브젝트 반환
-	private GameObject ClosestObject(string tag)
-	{
-		GameObject[] targetObjects = GameObject.FindGameObjectsWithTag(tag);
-		GameObject returnValue = null;
-		float minLength = float.MaxValue;
-
-		foreach (GameObject targetObject in targetObjects)
-		{
-			float lengthX = transform.position.x - targetObject.transform.position.x;
-			float lengthY = transform.position.y - targetObject.transform.position.y;
-			float newLength = Mathf.Sqrt(lengthX * lengthX + lengthY * lengthY);
-
-			if (newLength < minLength)
-			{
-				minLength = newLength;
-				returnValue = targetObject;
-			}
-		}
-
-		return returnValue;
-	}
-
+	
 	// 폭발 코루틴
 	private IEnumerator BombCountDown()
 	{
