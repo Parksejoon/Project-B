4bb5fd667eb8845a6587e83296ca16bc14639899
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-15 22:13:44 +0900

    Add teleportblock

---
+++ Assets/Scripts/Object/Item/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	// 인스펙터 비노출 변수
+	// 일반
+	private		Transform	playerTransform;			// 플레이어 트랜스폼
+
+
+	// 초기화
+	private void Awake()
+	{
+		playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+
+	// 시작
+	private void Start()
+	{
+		Vector3 newPosition = transform.position;
+
+		newPosition.y += transform.localScale.y / 2;
+		playerTransform.position = newPosition;
+	}
+}
