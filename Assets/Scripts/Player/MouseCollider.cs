1936bc30a702bb042e51f9fe3ec2970414ec7a95
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-26 22:23:39 +0900

    Add mouse collision effect

--- Assets/Scripts/Player/MouseCollider.cs
+++ Assets/Scripts/Player/MouseCollider.cs
@@ -2,11 +2,26 @@
 
 public class MouseCollider : MonoBehaviour
 {
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private GameObject	mouseBombEffect;
+
 	// 트리거 진입
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("DangerBlock"))
 		{
+			// 마우스 충돌 이펙트
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			mousePos.z = -9;
+
+			Instantiate(mouseBombEffect, mousePos, Quaternion.identity);
+
+			Debug.Log("ASD");
+
+			// 게임 종료
 			GameManager.instance.GameOver();
 		}
 	}
