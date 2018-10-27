75fc8f959d7add1110215e8e6e256186f526a593
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-27 22:48:04 +0900

    Add gravity bomb

---
+++ Assets/Scripts/Object/Item/Explosion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+	// 트리거 진입
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player") || collision.CompareTag("Ball"))
+		{
+			collision.GetComponent<Rigidbody2D>().AddForce((collision.transform.position - transform.position) * 5f, ForceMode2D.Impulse);
+		}
+
+		if (collision.CompareTag("BallGenerator"))
+		{
+			Destroy(collision.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Object/Item/GravityExplosion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GravityExplosion : MonoBehaviour
+{
+	// 트리거 진입
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player") || collision.CompareTag("Ball"))
+		{
+			collision.GetComponent<Rigidbody2D>().AddForce((transform.position - collision.transform.position) * 5f, ForceMode2D.Impulse);
+		}
+	}
+}
