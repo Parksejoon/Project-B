4a8dff56627bedf296ac20f578ea4dff154b4078
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-10-30 21:22:36 +0900

    Modify minor things

--- Assets/Scripts/System/ObjectPoolGenerator.cs
+++ Assets/Scripts/System/ObjectPoolGenerator.cs
@@ -6,20 +6,15 @@ public class ObjectPoolGenerator : MonoBehaviour
 	// 일반
 	[SerializeField]
 	private GameObject			ballPrefab;             // 공 프리팹
-	[SerializeField]
-	private GameObject			bombPrefab;				// 폭탄 프리팹
-
+	
 	// 수치
 	[SerializeField]
 	private int					ballSize;               // 공 크기
-	[SerializeField]
-	private int					bombSize;				// 폭탄 크기
 
 
 	// 초기화
 	private void Awake()
 	{
 		ObjectPoolManager.Create("Ball", ballPrefab, ballSize);
-		//ObjectPoolManager.Create("Bomb", ballPrefab, ballSize);
 	}
 }
