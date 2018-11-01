3e407e33547ed081b12e822bcac4182f1d53b2ad
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-11-01 16:53:21 +0900

    Add fairy system

--- Assets/Scripts/Player/MouseChase.cs
+++ Assets/Scripts/Player/MouseChase.cs
@@ -5,15 +5,26 @@ public class MouseChase : MonoBehaviour
 	// 인스펙터 노출 변수
 	// 수치
 	[SerializeField]
-	private	float		zPosition = 0;					// z 포지션
+	private	float		zPosition = 0;                  // z 포지션
+	[SerializeField]
+	private float		speed = 1;						// 속도
 
 
-	// 프레임
-	private void Update()
+	// 고정 프레임
+	private void FixedUpdate()
 	{
 		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 tempVector;
 
-		position.z = zPosition;
-		transform.position = position;
+		if (Vector2.Distance(transform.position, position) < 1)
+		{
+			tempVector = Vector2.Lerp(transform.position, new Vector2(position.x, position.y), Time.deltaTime * speed);
+		}
+		else
+		{
+			tempVector = Vector2.MoveTowards(transform.position, new Vector2(position.x, position.y), Time.deltaTime * speed);
+		}
+
+		transform.position = new Vector3(tempVector.x, tempVector.y, zPosition);
 	}
 }
--- Assets/Scripts/Player/MouseCollider.cs
+++ Assets/Scripts/Player/MouseCollider.cs
@@ -5,12 +5,17 @@ public class MouseCollider : MonoBehaviour
 	// 인스펙터 노출 변수
 	// 일반
 	[SerializeField]
-	private GameObject	mouseBombEffect;
+	private GameObject	mouseBombEffect;			// 마우스 폭발 이펙트
+	[SerializeField]
+	private MouseChase	mouseChase;					// 마우스 추적
 
-	// 트리거 진입
-	private void OnTriggerEnter2D(Collider2D collision)
+
+	// 충돌체 진입
+	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.CompareTag("DangerBlock"))
+		Debug.Log("ASD");
+		
+		if (collision.gameObject.CompareTag("DangerBlock"))
 		{
 			// 마우스 충돌 이펙트
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -19,10 +24,15 @@ public class MouseCollider : MonoBehaviour
 
 			Instantiate(mouseBombEffect, mousePos, Quaternion.identity);
 
-			Debug.Log("ASD");
 
 			// 게임 종료
 			GameManager.instance.GameOver();
 		}
+	}
+
+	// 충돌체 탈출
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+
 	}
 }
