7a3bc8cd0587dd8567b222a14220ddabcd5842ed
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-11-12 19:46:03 +0900

    Add pause key and pause menu

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -2,16 +2,35 @@
 
 public class Pause : MonoBehaviour
 {
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private		GameObject		pauseMenu;              // 일시정지 메뉴화면
+
+
+	// 프레임
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			PauseOrContinue();
+		}
+	}
+
 	// 정지
-	public void StopOrStart()
+	public void PauseOrContinue()
 	{
 		if (Time.timeScale == 0f)
 		{
 			Time.timeScale = 1f;
+
+			pauseMenu.SetActive(false);
 		}
 		else
 		{
 			Time.timeScale = 0f;
+
+			pauseMenu.SetActive(true);
 		}
 	}
 
