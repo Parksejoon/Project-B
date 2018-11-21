6149a29b6644def3c3e701cb4f90ebc668e0c682
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-11-21 20:49:49 +0900

    Add item generator

---
+++ Assets/Scripts/Object/Item/ItemGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	enum ItemType
+	{
+		Bomb,
+		GBomb,
+		PBomb,
+		Rack,
+		Teleport
+	};
+
+	[SerializeField]
+	private ItemType itemType;			// 아이템 종류
+
+	// 아이템들
+	public GameObject bomb;				// 폭탄
+	public GameObject g_bomb;			// 중력 폭탄
+	public GameObject p_bomb;			// 정화 폭탄
+	public GameObject rack;				// 점프대
+	public GameObject teleport;         // 텔레포트
+
+
+	// 시작
+	private void Start()
+	{
+		switch (itemType)
+		{
+			case ItemType.Bomb:
+				Instantiate(bomb, transform.position, Quaternion.identity, transform.parent);
+				break;
+			case ItemType.GBomb:
+				Instantiate(g_bomb, transform.position, Quaternion.identity, transform.parent);
+				break;
+			case ItemType.PBomb:
+				Instantiate(p_bomb, transform.position, Quaternion.identity, transform.parent);
+				break;
+			case ItemType.Rack:
+				Instantiate(rack, transform.position, Quaternion.identity, transform.parent);
+				break;
+			case ItemType.Teleport:
+				Instantiate(teleport, transform.position, Quaternion.identity, transform.parent);
+				break;
+			default:
+				break;
+		}
+
+		Destroy(gameObject);
+	}
+}
