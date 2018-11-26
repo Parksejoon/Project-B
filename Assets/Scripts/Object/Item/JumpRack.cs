e378a0cf47a2539c6d0850818d5decd91fb602eb
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-11-26 20:01:13 +0900

    Add jump rack jump effect

--- Assets/Scripts/Object/Item/JumpRack.cs
+++ Assets/Scripts/Object/Item/JumpRack.cs
@@ -2,6 +2,11 @@
 
 public class JumpRack : MonoBehaviour
 {
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private			GameObject		jumpEffect;			// 점프 파티클
+
 	// 인스펙터 비노출 변수
 	// 일반
 	private const	float			jumpPower = 10;     // 점프 파워
@@ -13,6 +18,7 @@ public class JumpRack : MonoBehaviour
 	{
 		if (collision.CompareTag("Player"))
 		{
+			// 물리
 			Vector3 rotation = transform.parent.rotation.eulerAngles;
 			float angle = rotation.z * Mathf.PI / 180;
 
@@ -22,6 +28,14 @@ public class JumpRack : MonoBehaviour
 
 			targetRigidbody.velocity = Vector2.zero;
 			targetRigidbody.AddForce(jumpWay * jumpPower, ForceMode2D.Impulse);
+
+			// 파티클
+			Vector3 position = transform.position;
+
+			position.z = -15;
+			position.y += 0.5f;
+
+			Instantiate(jumpEffect, position, Quaternion.identity);
 		}
 	}
 }
