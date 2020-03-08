7e972b34701b6af20e763a5dfdb20e0755b14ba5
LAPTOP-SoonSem <qkeh4161@naver.com>
2020-03-08 15:14:01 +0900

    Separate Depth class from GameManager script.

---
+++ Assets/Scripts/Systems/Depth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+## sprite z value ##
+
+ -4.5 CustomBlock
+ -5 Base
+ -6 Monster
+ -7 Player
+ -7.5 Effect
+ -8 Fairy & Light
+
+ */
+
+
+public static class Depth
+{
+	public const float CustomBlock = -4.5f;
+	public const float Base = -5f;
+	public const float Monster = -6f;
+	public const float Player = -7f;
+	public const float Effect = -7.5f;
+	public const float Fairy = -8f;
+	public const float Light = -8f;
+}
--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -37,27 +37,3 @@ public class GameManager : MonoBehaviour
 	}
 }
 
-/*
-
-## sprite z value ##
-
- -4.5 CustomBlock
- -5 Base
- -6 Monster
- -7 Player
- -7.5 Effect
- -8 Fairy & Light
-
- */
- 
-
-public static class Depth
-{
-	public const float CustomBlock	= -4.5f;
-	public const float Base			= -5f;
-	public const float Monster		= -6f;
-	public const float Player		= -7f;
-	public const float Effect		= -7.5f;
-	public const float Fairy		= -8f;
-	public const float Light		= -8f;
-}
