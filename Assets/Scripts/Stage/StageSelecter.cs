e578b4c4990b9d2c2d7dbbe4de5d251f0d6f376a
LAPTOP-SoonSem <qkeh4161@naver.com>
2020-04-09 15:05:11 +0900

    Add stage system base.

---
+++ Assets/Scripts/Stage/StageSelecter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelecter : MonoBehaviour
+{
+	// 꿈 시작
+	public void StartDream()
+	{
+		SceneManager.LoadScene("MainFightScene");
+	}
+}
